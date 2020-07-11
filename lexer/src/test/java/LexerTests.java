15e0f036c702639084fb09fa526f3a86956e7007
Mariano Longo <mariano.longo@ing.austral.edu.ar>
2020-07-10 22:55:03 -0300

    added lexer tests

---
+++ lexer/src/test/java/LexerTests.java
@@ -0,0 +1,49 @@
+import exceptions.LexerException;
+import logic.Lexer;
+import logic.impl.LexerImpl;
+import org.junit.Assert;
+import org.junit.Test;
+import token.Token;
+import token.type.TokenType;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public class LexerTests {
+
+    private Lexer lexer = new LexerImpl();
+
+    @Test
+    public void test_01_testSuccessfulLexWithCorrectTokens(){
+        List<Token> tokens = lexer.getTokens(new InputStreamReader(new ByteArrayInputStream(
+                ("let a: string = \"This is a test!\";").getBytes())));
+
+        Assert.assertEquals(8, tokens.size());
+
+        Assert.assertSame(tokens.get(0).getType(), TokenType.LET);
+        Assert.assertSame(tokens.get(1).getType(), TokenType.IDENTIFIER);
+        Assert.assertSame(tokens.get(2).getType(), TokenType.COLON);
+        Assert.assertSame(tokens.get(3).getType(), TokenType.STRING);
+        Assert.assertSame(tokens.get(4).getType(), TokenType.EQUAL);
+        Assert.assertSame(tokens.get(5).getType(), TokenType.STRING);
+        Assert.assertEquals("This is a test!", tokens.get(5).getLiteral());
+        Assert.assertSame(tokens.get(6).getType(), TokenType.SEMICOLON);
+    }
+
+    @Test(expected = LexerException.class)
+    public void test_02_unterminatedStringThrowsLexerException(){
+        lexer.getTokens(new InputStreamReader(new ByteArrayInputStream(
+                ("let a: string = \"This string does not end;").getBytes())));
+    }
+
+    @Test
+    public void test_03_newLineIsCorrectlyImplemented(){
+        List<Token> tokens = lexer.getTokens(new InputStreamReader(new ByteArrayInputStream(
+                ("let a: string = \"This is a test!\"; \n" +
+                        "const b: boolean = true;").getBytes())));
+
+        Assert.assertEquals(Integer.valueOf(1), tokens.get(0).getLine());
+        Assert.assertEquals(Integer.valueOf(2), tokens.get(8).getLine());
+    }
+}
--- src/main/resources/hello.ts
+++ src/main/resources/hello.ts
@@ -1,17 +1,1 @@
-let a: string = "Hello World!";
-print (a);
-
-a = "Hola Mundo!";
-print(a);
-
-const b: number = 2;
-
-print(b);
-
-const condition: boolean = true;
-if(condition) {
-    print("Paso el if");
-} else {
-    print("No paso el if");
-}
-
+let b: string = "asd";
